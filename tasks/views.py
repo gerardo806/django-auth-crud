4c767834d9b07be5f39f5665f4cc102b5a42285f
zam <zam23pc@gmail.com>
2022-09-23 01:14:29 -0600

    nav config

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+    
+def signup(request):
+    msg = ""
+    
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        confirm = request.POST["password2"]
+        
+        if password == confirm:
+            try:
+                user = User.objects.create_user(username = username, password = password)
+                user.save()
+                #msg = "user created successfully"
+                login(request, user)
+                return redirect("tasks")
+            except IntegrityError:
+                msg ="El usuario ya existe!"
+        else:
+            msg = "Las claves no son iguales"
+    
+    return render(request, "signup.html", {
+        "form": UserCreationForm, "msg": msg
+    })
+
+def tasks(request):
+    return render(request, "tasks.html")
