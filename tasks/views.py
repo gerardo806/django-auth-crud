3449ae163aa7afb6733b242e99d159081c409868
zam <zam23pc@gmail.com>
2023-02-13 13:27:15 -0600

    add task detail

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
@@ -7,65 +7,69 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 
+
 # Create your views here.
 def home(request):
     return render(request, "home.html")
-    
+
+
 def signup(request):
     msg = ""
-    
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password1"]
         confirm = request.POST["password2"]
-        
+
         if password == confirm:
             try:
-                user = User.objects.create_user(username = username, password = password)
+                user = User.objects.create_user(username=username, password=password)
                 user.save()
-                #msg = "user created successfully"
+                # msg = "user created successfully"
                 login(request, user)
                 return redirect("tasks")
             except IntegrityError:
-                msg ="El usuario ya existe!"
+                msg = "El usuario ya existe!"
         else:
             msg = "Las claves no son iguales"
-    
-    return render(request, "signup.html", {
-        "form": UserCreationForm, "msg": msg
-    })
+
+    return render(request, "signup.html", {"form": UserCreationForm, "msg": msg})
+
 
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, "tasks.html", {
-        "tasks": tasks
-    })
+    return render(request, "tasks.html", {"tasks": tasks})
+
+
+def task_detail(request, task_id):
+    # pk: primary key
+    # obtener un registro o enviar un 404 -> get_object_or_404
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, "task_detail.html", {"task": task})
+
 
 def signout(request):
     logout(request)
     return redirect("home")
 
+
 def signin(request):
     msg: str = ""
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        
-        user = authenticate(
-            request, 
-            username=username, 
-            password= password)
-        
-        if user is None: msg = "Username or password is incorrect"
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            msg = "Username or password is incorrect"
         else:
-            login(request, user) 
+            login(request, user)
             return redirect("tasks")
-        
-    return render(request, "signin.html", {
-        "form": AuthenticationForm,
-        "msg": msg
-    })
-    
+
+    return render(request, "signin.html", {"form": AuthenticationForm, "msg": msg})
+
+
 def create_task(request):
     msg = ""
     if request.method == "POST":
@@ -77,8 +81,5 @@ def create_task(request):
             return redirect("tasks")
         except ValueError:
             msg = "Ingrese datos validos"
-    
-    return render(request, "create_task.html", {
-        "form": TaskForm,
-        "msg": msg
-    })
+
+    return render(request, "create_task.html", {"form": TaskForm, "msg": msg})
